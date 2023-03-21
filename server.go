11083dac4ea97a341f93a82a11264b3f44591353
Dmitriy Pervin <theforner@gmail.com>
2023-03-22 02:20:39 +0500

    Refactor

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -22,23 +23,30 @@ func main() {
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&Question{})
+	/*	db.AutoMigrate(&Locale{})
+		db.AutoMigrate(&Category{})
+		db.AutoMigrate(&Question{})
+		db.AutoMigrate(&Answer{})*/
 
-	// Echo init∏
+	// Echo init
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
-	//e.Use(middleware.Recover())
+	e.Use(middleware.Recover())
+
 	e.Static("/static", "static")
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
 	})
-	e.POST("/quiz/", postAddQuestion)
-	e.GET("/quiz/quess_movie", getQuessMovie)
+	//e.POST("/questions", postQuestion) // TODO
+	e.GET("/questions", getQuestions)
+	e.GET("/categories", getCategories)
+	e.GET("/locales", getLocales)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func postAddQuestion(c echo.Context) (err error) {
+func postQuestion(c echo.Context) (err error) {
 	question := new(Question)
 	if err = c.Bind(question); err != nil {
 		return c.String(http.StatusBadRequest, "bad request")
@@ -51,12 +59,60 @@ func postAddQuestion(c echo.Context) (err error) {
 	return c.JSON(http.StatusOK, question)
 }
 
-func getQuessMovie(c echo.Context) error {
-	var question Question
+func getQuestions(c echo.Context) error {
+	var question []Question
 
-	if result := db.Order("RANDOM()").Limit(1).First(&question); result.Error != nil {
+	query := db.Preload("Answers").Order("RANDOM()")
+
+	// Category
+	categoryId, err := strconv.Atoi(c.QueryParam("category_id"))
+	if err == nil && categoryId != 0 {
+		query = query.Where("category_id = ?", categoryId)
+	}
+
+	// Question type
+	typeId, err := strconv.Atoi(c.QueryParam("type_id"))
+	if err == nil && typeId != 0 {
+		query = query.Where("type_id = ?", typeId)
+	}
+
+	// Locale
+	localeCode := c.QueryParam("locale_code")
+	if localeCode != "" {
+		query = query.Where("locale_code = ?", localeCode)
+	}
+
+	// Limit
+	limit, err := strconv.Atoi(c.QueryParam("limit"))
+	if err == nil && limit != 0 {
+		query = query.Limit(limit)
+	} else {
+		query = query.Limit(10)
+	}
+
+	if result := query.Debug().Find(&question); result.Error != nil {
 		return c.JSON(http.StatusInternalServerError, "DB ERR")
 	}
 
 	return c.JSON(http.StatusOK, question)
+}
+
+func getCategories(c echo.Context) error {
+	var categories []Category
+
+	if result := db.Find(&categories); result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, "DB ERR")
+	}
+
+	return c.JSON(http.StatusOK, categories)
+}
+
+func getLocales(c echo.Context) error {
+	var locales []Locale
+
+	if result := db.Find(&locales); result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, "DB ERR")
+	}
+
+	return c.JSON(http.StatusOK, locales)
 }
--- types.go
+++ types.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"gorm.io/gorm"
 	"time"
 )
 
 type Question struct {
-	ID        uint      `gorm:"primaryKey,autoIncrement"`
-	Type      uint      `gorm:"index" form:"type"`
-	Question  string    `form:"question"`
-	MediaUrl  string    `form:"media_url"`
-	MediaType uint      `form:"media_type"`
-	Locale    string    `gorm:"index" form:"locale"`
-	Answer    string    `form:"answer"`
-	Answer1   string    `form:"answer1"`
-	Answer2   string    `form:"answer2"`
-	Answer3   string    `form:"answer3"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	ID         uint           `json:"id" gorm:"primaryKey,autoIncrement"`
+	CategoryID int            `json:"category_id" form:"category_id"`
+	Category   Category       `json:"-" form:"category"`
+	TypeID     int            `json:"type_id" form:"type_id" gorm:"index"`
+	Text       string         `json:"question" form:"question"`
+	MediaType  int            `json:"media_type" form:"media_type"`
+	MediaUrl   string         `json:"media_url" form:"media_url"`
+	LocaleCode string         `json:"locale_code" form:"locale_code"`
+	Locale     Locale         `json:"-" form:"locale" gorm:"references:Code" gorm:"foreignKey:LocaleCode"`
+	Answers    []Answer       `json:"answers" gorm:"foreignKey:QuestionID"`
+	CreatedAt  time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt  time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+}
+
+type Category struct {
+	ID   int    `json:"id" gorm:"primaryKey,autoIncrement"`
+	Text string `json:"text" form:"text"`
+}
+
+type Answer struct {
+	ID         int      `json:"id" gorm:"primaryKey,autoIncrement"`
+	QuestionID int      `json:"question_id" form:"question_id"`
+	Question   Question `json:"-"`
+	Text       string   `json:"text" form:"text"`
+	Valid      bool     `json:"is_valid" form:"is_valid"`
+}
+
+type Locale struct {
+	Code     string `json:"code" gorm:"primaryKey,autoIncrement"`
+	Language string `json:"language"`
+	Country  string `json:"country"`
 }
